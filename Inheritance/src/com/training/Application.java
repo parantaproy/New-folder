cf4b97dc16b7bb2c8718b50c15bc748c5adae293
Parantap Roy <parantaproy@gmail.com>
2016-06-29 16:37:32 +0530

    Inheritance

---
+++ Inheritance/src/com/training/Application.java
@@ -0,0 +1,15 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+    Book bk= new Book(101,"Head First java",350.00);
+    
+    System.out.println(bk);
+    
+    
+	}
+
+}
